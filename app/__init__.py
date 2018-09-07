369cbc613ab70aaaedd3bdf7e49a2841fdbc1b12
feysal-Ibrahim <addictivefazman@gmail.com>
2018-09-08 00:37:50 +0300

    initialize the flask extensios in the app factory settting

--- app/__init__.py
+++ app/__init__.py
@@ -28,14 +28,14 @@ def create_app(config_name):
     app.register_blueprint ( auth_blueprint , url_prefix='/auth' )
 
 
-    # # Initializing flask extensions
-    # bootstrap.init_app(app)
-    # db.init_app(app)
-    # login_manager.iniit_app(app)
-    #
-    # # Registering the blueprint
-    # from .main import main as main_blueprint
-    # app.register_blueprint ( main_blueprint )
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.iniit_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint ( main_blueprint )
 
     # Will add the views and forms
 

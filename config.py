d326ac67e9dd82322892d69490bfb980cc894adb
feysal-Ibrahim <addictivefazman@gmail.com>
2018-09-08 00:41:08 +0300

    place the sqlalchemy url in the config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class ProdConfig(Config):
 class DevConfig(Config):
     DEBUG = True
 
-config_options = {
-'development':DevConfig,
-'production':ProdConfig
-}
+# config_options = {
+# 'development':DevConfig,
+# 'production':ProdConfig
+# }

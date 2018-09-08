fbf26afac596b7d44c10720abc24d5d403f39cdd
feysal-Ibrahim <addictivefazman@gmail.com>
2018-09-08 23:16:22 +0300

    create class user

--- app/models.py
+++ app/models.py
@@ -7,14 +7,14 @@ from . import login_manager
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(UserMixin,db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-    email = db.Column ( db.String ( 255 ) , unique=True , index=True )
-    pass_secure = db.Column ( db.String ( 255 ) )
-    # Defining the One to many relationship between a user and a pitch
-    pitch = db.relationship ( 'Pitch' , backref="user" , lazy='dynamic' )
+# class User(UserMixin,db.Model):
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer,primary_key = True)
+#     username = db.Column(db.String(255))
+#     email = db.Column ( db.String ( 255 ) , unique=True , index=True )
+#     pass_secure = db.Column ( db.String ( 255 ) )
+#     # Defining the One to many relationship between a user and a pitch
+#     pitch = db.relationship ( 'Pitch' , backref="user" , lazy='dynamic' )
 
     @property
     def password(self):

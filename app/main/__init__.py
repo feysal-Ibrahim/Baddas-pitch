d76e4af91ed63118c298a7e71c1625939db5582d
feysal-Ibrahim <addictivefazman@gmail.com>
2018-09-09 01:05:19 +0300

    import views.py,errors.py and form.py from main folder in the app folder

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,9 @@
-# from flask import Blueprint
-# main=Blueprint('main',__name__)
-# from . import views,error,forms
+from flask import Blueprint
+main=Blueprint('main',__name__)
+'''
+create an instance of the main and set it as blueprint
+'''
+from . import views,error,forms
+'''
+import views.py,errors.py and form.py from main folder in the app folder
+'''

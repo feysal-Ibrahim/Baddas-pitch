d6fb6194493c87f7bd7bccf614070264b1db3193
feysal-Ibrahim <addictivefazman@gmail.com>
2018-09-09 17:15:15 +0300

    then create a new configuration UPLOADED_PHOTOS_DEST which specifies the destination to where we want to store our Images

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://feisal:1234@localhost/pitch'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
 
 class ProdConfig(Config):
